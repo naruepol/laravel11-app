f2a623bebd5cab304730211e617f819fe75f5a1b
naruepol <naruapon@gmail.com>
2024-05-06 16:50:36 +0700

    refactor: Update Pagination component to highlight active link

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -3,21 +3,14 @@
       <div class="flex flex-wrap -mb-1">
         <template v-for="(link, key) in links">
           <div v-if="link.url === null" :key="key" class="px-4 py-3 mb-1 mr-1 text-sm leading-4 text-gray-400 border rounded" v-html="link.label" />
-          <Link v-else :key="`link-${key}`" class="px-4 py-3 mb-1 mr-1 text-sm leading-4 border rounded focus:text-indigo-500 hover:bg-white focus:border-indigo-500" :class="{ 'bg-white': link.active }" :href="link.url" v-html="link.label" />
+          <Link v-else :key="`link-${key}`" class="px-4 py-3 mb-1 mr-1 text-sm leading-4 border rounded focus:text-indigo-500 hover:bg-white focus:border-indigo-500" :class="{ 'bg-indigo-500': link.active }" :href="link.url" v-html="link.label" />
         </template>
       </div>
     </div>
   </template>
-
   <script setup>
   import { Link } from '@inertiajs/vue3'
-
-  export default {
-    components: {
-      Link,
-    },
-    defineProps: {
-      links: Array,
-    },
-  }
+  defineProps({
+    links: Array
+  });
   </script>
