cef228077d8671eb9d4035171539340dd0572365
naruepol <naruapon@gmail.com>
2024-05-06 10:05:44 +0700

    chore: Add Tailwind CSS configuration files and components

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,27 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-//http://localhost/laravel11-app/public/
-//http://laravel11-app.local/about
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
-//http://localhost/laravel11-app/public/about
-//http://laravel11-app.local/about
-Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
