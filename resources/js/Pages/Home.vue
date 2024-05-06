6d14b2c691020dd6ef9562f0a429ef482e2eb6ee
naruepol <naruapon@gmail.com>
2024-05-06 11:50:59 +0700

    refactor: Add info alert to home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,8 +1,11 @@
 <script setup>
-    import { Head } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
+
     <Head title="หน้าหลัก" />
-    <p>Hello</p>
+    <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+        <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+    </div>
 </template>
